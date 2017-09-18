eaa81156991b3a6cfc34aedb3b6f0dba068add19
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-09-18 11:29:13 +0200

    skriva info om mig

---
+++ ovning2_1.java
@@ -0,0 +1,22 @@
+import javax.swing.*;
+
+
+public class ovning2_1{ 
+	public static void main(String[] args) {
+
+		String name;
+			name = "Mustafa";
+
+		String adress;
+			adress = "Kyrkogatan 10 Nordmaling";	
+
+		String number;
+			number = "0703836656";
+
+		String Personelinformation;
+			Personelinformation = name+" "+adress+" "+number;
+
+JOptionPane.showMessageDialog(null, name+" "+adress+" "+number+" "+Personelinformation);			
+
+}		
+	}
