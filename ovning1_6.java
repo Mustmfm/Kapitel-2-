6f5b0a3286079cff4e30425b8dbfa249b7276226
Mustafa Abdulrazzaq <abmu16@nti.umea>
2017-09-18 10:24:28 +0200

    Program som skriver namn och nummer

---
+++ ovning1_6.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+/*
+Vad?
+Vem?
+När?
+ */
+
+public class ovning1_6 {
+	public static void main (String[] arg){
+		JOptionPane.showMessageDialog(null, "0703836656");
+		JOptionPane.showMessageDialog(null, "Mustafa");
+
+
+	}
+}
